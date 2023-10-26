4b3a5d9ba3a038777fcb5c2f22412decb461985e
Manuel Alanis <manuelaag99@gmail.com>
2023-10-25 19:06:00 -0600

    add conditional render to videoplayer

--- src/app/Components/VideoPlayer.jsx
+++ src/app/Components/VideoPlayer.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react"
+
 export default function VideoPlayer ({ additionalClassNames, videoSource, videoWidth }) {
-    return (
-        <div className={"w-full" + additionalClassNames}>
-            <video controls width={videoWidth} >
-                <source src={videoSource} type="video/mp4" />
-                Your browser does not support this video tag.
-            </video>
-        </div>
-    )
+    const [videoToPlay, setVideoToPlay] = useState();
+
+    if (!videoToPlay) {
+        return null;
+    } else {
+        return (
+            <div className={"w-full" + additionalClassNames}>
+                <video controls width={videoWidth} >
+                    <source src={videoToPlay} type="video/mp4" />
+                    Your browser does not support this video tag.
+                </video>
+            </div>
+        )
+    }
 }
--- src/app/Pages/CourseVideoPage.jsx
+++ src/app/Pages/CourseVideoPage.jsx
@@ -1,7 +1,9 @@
+import VideoPlayer from "../Components/VideoPlayer";
+
 export default function CourseVideoPage ({}) {
     return (
         <div className="w-full ">
-
+            <VideoPlayer additionalClassNames="h-96" videoSource={selectedVideo} videoWidth="400" />
         </div>
     )
 }

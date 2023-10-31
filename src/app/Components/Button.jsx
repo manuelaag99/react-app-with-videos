974e11a3bad91537e62fdb24653d38bd71c968d4
Manuel Alanis <manuelaag99@gmail.com>
2023-10-30 20:34:10 -0600

    create custom button component

---
+++ src/app/Components/Button.jsx
@@ -0,0 +1,9 @@
+export default function Button ({ additionalClassNamesForButton, additionalClassNamesForText, onClickButtonAction, contentForButton }) {
+    return (
+        <button className={"py-1 px-3 " + additionalClassNamesForButton} onClick={onClickButtonAction}>
+            <p className={" " + additionalClassNamesForText}>
+                {contentForButton}
+            </p>
+        </button>
+    )
+}

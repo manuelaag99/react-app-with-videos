92f549115daea11eb09493d1e22326e6be03ecd1
Manuel Alanis <manuelaag99@gmail.com>
2023-11-01 20:46:44 -0600

    fix course video page

--- src/app/Components/TopNavigationBar.jsx
+++ src/app/Components/TopNavigationBar.jsx
@@ -2,34 +2,34 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 export default function TopNavigationBar ({}) {
     return (
-        <div className="flex flex-col w-full h-fit px-6 py-2 bg-var-1 fixed top-0 shadow-xl">
+        <div className="flex flex-col w-full h-fit px-3 py-2 bg-var-1 fixed top-0 shadow-xl">
             <div className="flex flex-row justify-between w-full h-fit">
-                <div className="flex justify-center items-center w-2/10">
-                    <p className="text-white hover:text-var-1-hovered duration-200">
+                <div className="flex justify-start items-center w-fit sm:hidden ">
+                    <button className="text-white hover:text-var-1-hovered duration-200 cursor-pointer">
                         <MenuIcon color="#fff" fontSize='large' />
-                    </p>
+                    </button>
                 </div>
-                <div className="flex justify-start sm:justify-center w-1/10 sm: w-6/10">
+                <div className="flex justify-start sm:justify-center w-4/10 sm:w-6/10">
                     <img src="" alt="" />
                 </div>
                 <div className="sm:flex flex-row w-fit hidden">
                     <button className="p-3 ">
-                        <p className="text-white hover:text-var-1-hovered duration-200">
+                        <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap">
                             Hacer un pedido
                         </p>
                     </button>
                     <button className="p-3 ">
-                        <p className="text-white hover:text-var-1-hovered duration-200">
+                        <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap">
                             Cursos
                         </p>
                     </button>
                     <button className="p-3 ">
-                        <p className="text-white text-var-1-hovered duration-200">
+                        <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap">
                             Registrarse
                         </p>
                     </button>
                     <button className="p-3 ">
-                        <p className="text-white hover:text-var-1-hovered duration-200">
+                        <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap">
                             Iniciar sesión
                         </p>
                     </button>

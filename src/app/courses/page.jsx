117bd19c93cb307106452274c45bdf9d68a3a781
Manuel Alanis <manuelaag99@gmail.com>
2023-11-05 21:43:25 -0600

    modify courses page

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,9 +1,15 @@
 "use client"
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import TopNavigationBar from '../Components/TopNavigationBar';
+import { useState } from 'react';
 
 export default function CoursePage ({}) {
+    const [courseModules, setCourseModules] = useState();
+
     return (
         <div className="flex flex-col justify-center w-full">
+            <TopNavigationBar />
             <div className="flex w-9/10 mx-auto justify-center bg-white rounded-md mt-14">
                 <div className="flex flex-row w-full">
                     <div className="flex flex-col w-1/2 px-5 py-7 items-center">
@@ -24,8 +30,34 @@ export default function CoursePage ({}) {
                         <img src="" alt="" />
                     </div>
                 </div>
-                <div className="">
-                </div>
+                {courseModules && <div className="flex flex-col justify-center w-full">
+                    {courseModules.map((index, module) => {
+                        return (
+                            <div className="flex flex-row justify-center w-full py-3" key={index}>
+                                <div className="flex justify-center items-center w-2/10 h-12">
+                                    <img className='' src="" alt="" />
+                                </div>
+                                <div className="flex flex-col justify-start items-start w-7/10">
+                                    <div className="flex w-full">
+                                        <p className="text-left font-bold">
+                                            Módulo 1: {module.module_name}
+                                        </p>
+                                    </div>
+                                    <div className="flex w-full">
+                                        <p className="text-left">
+                                            {module.module_description}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="flex justify-end items-center w-1/10 ">
+                                    <button>
+                                        <MoreHorizIcon />
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>}
             </div>
         </div>
     )

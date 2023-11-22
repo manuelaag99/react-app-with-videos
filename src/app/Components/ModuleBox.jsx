c8e24c38421a2cbe253053cb153bef43b77335e5
Manuel Alanis <manuelaag99@gmail.com>
2023-11-21 19:29:48 -0600

    responsive module box design

--- src/app/Components/ModuleBox.jsx
+++ src/app/Components/ModuleBox.jsx
@@ -27,26 +27,35 @@ export default function ModuleBox ({ index, moduleId }) {
         return null;
     } else {
         return (
-            <div className="flex flex-row justify-center w-full py-5 px-6 hover:bg-gray-300 shadow-lg duration-300" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
-                <div className="flex justify-center items-center w-2/10 h-24">
+            <div className="flex sm:flex-row flex-col justify-center w-full py-5 px-6 hover:bg-gray-300 shadow-lg duration-300" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
+                <div className="flex justify-center items-center w-full sm:w-2/10 h-24">
                     {moduleInfo && <Thumbnail thumbnailImageId={moduleInfo.module_image_path} />}
                     {!moduleInfo && <div className="bg-gray-400 w-full h-full"></div>}
                 </div>
-                <div className="flex flex-col justify-start items-start w-7/10 pl-6">
-                    <div className="flex w-full">
-                        {moduleInfo && <p className="text-left font-bold">
-                            Módulo {index}: {moduleInfo.module_title}
-                        </p>}
-                        {!moduleInfo && <div className="bg-gray-400 w-5/10 h-16"></div>}
+                <div className="flex flex-row sm:flex-col justify-start items-start w-full sm:w-7/10 sm:pl-6">
+                    <div className="flex flex-col w-9/10 ">
+                        <div className="flex w-full">
+                            {moduleInfo && <p className="text-left font-bold">
+                                Módulo {index}: {moduleInfo.module_title}
+                            </p>}
+                            {!moduleInfo && <div className="bg-gray-400 w-5/10 h-16"></div>}
+                        </div>
+                        <div className="flex w-full">
+                            {moduleInfo && <p className="text-left opacity-70">
+                                {moduleInfo.module_description.slice(0,80)}...
+                            </p>}
+                            {!moduleInfo && <div className="bg-gray-400 w-8/10 h-16"></div>}
+                        </div>
                     </div>
-                    <div className="flex w-full">
-                        {moduleInfo && <p className="text-left opacity-70">
-                            {moduleInfo.module_description.slice(0,80)}...
-                        </p>}
-                        {!moduleInfo && <div className="bg-gray-400 w-8/10 h-16"></div>}
+
+                    <div className="sm:hidden flex justify-end items-center w-1/10 ">
+                        <button>
+                            <MoreVertIcon />
+                        </button>
                     </div>
+
                 </div>
-                <div className="flex justify-end items-center w-1/10 ">
+                <div className="hidden sm:flex justify-end items-center w-1/10 ">
                     <button>
                         <MoreVertIcon />
                     </button>

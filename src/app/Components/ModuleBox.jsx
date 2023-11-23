088bd116d7db1aa6e0a67fdd71cd76ad203cca33
Manuel Alanis <manuelaag99@gmail.com>
2023-11-22 18:38:38 -0600

    add hover to button in module box

--- src/app/Components/ModuleBox.jsx
+++ src/app/Components/ModuleBox.jsx
@@ -27,16 +27,17 @@ export default function ModuleBox ({ index, moduleId }) {
         return null;
     } else {
         return (
-            <div className="flex sm:flex-row flex-col justify-center w-full py-5 px-6 hover:bg-gray-300 shadow-lg duration-300" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
+            <div className="flex sm:flex-row flex-col justify-center w-full py-5 px-6 hover:bg-gray-300 hover:shadow-lg duration-300 cursor-pointer" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
                 <div className="flex justify-center items-center w-full sm:w-2/10 h-24">
                     {moduleInfo && <Thumbnail thumbnailImageId={moduleInfo.module_image_path} />}
                     {!moduleInfo && <div className="bg-gray-400 w-full h-full"></div>}
                 </div>
-                <div className="flex flex-row sm:flex-col justify-start items-start w-full sm:w-7/10 sm:pl-6">
-                    <div className="flex flex-col w-9/10 ">
+                <div className="flex flex-row sm:flex-col justify-center w-full sm:w-7/10 sm:pl-6">
+                    
+                    <div className="flex flex-col w-9/10 pr-6 ">
                         <div className="flex w-full">
                             {moduleInfo && <p className="text-left font-bold">
-                                Módulo {index}: {moduleInfo.module_title}
+                                Módulo {index + 1}: {moduleInfo.module_title}
                             </p>}
                             {!moduleInfo && <div className="bg-gray-400 w-5/10 h-16"></div>}
                         </div>
@@ -48,15 +49,15 @@ export default function ModuleBox ({ index, moduleId }) {
                         </div>
                     </div>
 
-                    <div className="sm:hidden flex justify-end items-center w-1/10 ">
-                        <button>
+                    <div className="sm:hidden flex justify-end items-center w-1/10 mt-1 ">
+                        <button className="text-black hover:text-gray-100 duration-300">
                             <MoreVertIcon />
                         </button>
                     </div>
 
                 </div>
                 <div className="hidden sm:flex justify-end items-center w-1/10 ">
-                    <button>
+                    <button className="text-black hover:text-gray-100 duration-300">
                         <MoreVertIcon />
                     </button>
                 </div>

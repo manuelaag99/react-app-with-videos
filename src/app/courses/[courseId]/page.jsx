bdea8c05f08966b52556328a601ccc49066ef24f
Manuel Alanis <manuelaag99@gmail.com>
2023-11-24 19:53:18 -0600

    add loading phase to specific bits of course page info

--- src/app/courses/[courseId]/page.jsx
+++ src/app/courses/[courseId]/page.jsx
@@ -33,51 +33,44 @@ export default function CoursePage ({ params }) {
         fetchModuleIds();
     }, [])
 
-    if (!courseInfo || !courseModules) {
-        return null;
-    } else {
-        return (
-            <div className="flex flex-col justify-center w-full">
-                <TopNavigationBar />
-                <div className="flex flex-col w-85percent mx-auto justify-center bg-white rounded-md mt-28 mb-28 pb-24">
-                    
-                    <div className="flex sm:flex-row flex-col-reverse sm:justify-between justify-center w-full p-6">
-                        <div className="flex flex-col sm:w-46percent w-full items-center mb-14">
-                            <div className="flex flex-col w-full justify-start mb-10">
-                                <div className="flex w-full mb-6">
-                                    <p className="font-bold text-left pr-6">
-                                        {courseInfo.course_title}
-                                    </p>
-                                </div>
-                                <div className="flex w-full mb-6">
-                                    <p className="text-left pr-6">
-                                        {courseInfo.course_description}
-                                    </p>
-                                </div>
+    return (
+        <div className="flex flex-col justify-center w-full">
+            <TopNavigationBar />
+            <div className="flex flex-col w-85percent mx-auto justify-center bg-white rounded-md mt-28 mb-28 pb-24">
+                
+                <div className="flex sm:flex-row flex-col-reverse sm:justify-between justify-center w-full p-6">
+                    <div className="flex flex-col sm:w-46percent w-full items-center mb-14">
+                        <div className="flex flex-col w-full justify-start mb-10">
+                            <div className="flex w-full mb-6">
+                                {!courseInfo && <div className="rounded-2xl bg-gray-700 w-4/10 h-7 mt-1"></div>}
+                                {courseInfo && <p className="font-bold text-left pr-6">{courseInfo.course_title}</p>}
                             </div>
-                            <div className="flex w-full justify-start">
-                                <div className="flex w-full justify-start">
-                                    <p className="text-left opacity-40">
-                                        <AccessTimeIcon fontSize="large" />
-                                    </p>
-                                </div>
+                            <div className="flex w-full mb-6">
+                                {!courseInfo && <div className="rounded-lg bg-gray-400 w-7/10 h-5 mt-1"></div>}
+                                {courseInfo && <p className="text-left pr-6">{courseInfo.course_description}</p>}
                             </div>
                         </div>
-    
-                        <div className="flex flex-col sm:w-46percent w-full">
-                            <img src="" alt="" />
+                        <div className="flex w-full justify-start">
+                            <div className="flex flex-row w-full justify-start">
+                                <p className="text-left text-gray-400 mr-2"><AccessTimeIcon fontSize="medium" /></p>
+                                {!courseInfo && <div className="rounded-lg bg-gray-300 w-3/10 h-5 mt-1"></div>}
+                            </div>
                         </div>
                     </div>
 
-                    {courseModules && (courseModules.length > 0) && <div className="flex flex-col justify-center w-full">
-                        {courseModules.map((module, index) => {
-                            return (
-                                <ModuleBox index={index} key={index} moduleId={module.module_id} />
-                            )
-                        })}
-                    </div>}
+                    <div className="flex flex-col sm:w-46percent w-full">
+                        <img src="" alt="" />
+                    </div>
                 </div>
+
+                {courseModules && (courseModules.length > 0) && <div className="flex flex-col justify-center w-full">
+                    {courseModules.map((module, index) => {
+                        return (
+                            <ModuleBox index={index} key={index} moduleId={module.module_id} />
+                        )
+                    })}
+                </div>}
             </div>
-        )
-    }
+        </div>
+    )
 }

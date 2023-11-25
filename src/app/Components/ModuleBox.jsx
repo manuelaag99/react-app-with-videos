8d37a3f575d5cc8b801758caec0b7342e2dcd3f8
Manuel Alanis <manuelaag99@gmail.com>
2023-11-24 20:00:00 -0600

    add loading state to module box

--- src/app/Components/ModuleBox.jsx
+++ src/app/Components/ModuleBox.jsx
@@ -23,45 +23,33 @@ export default function ModuleBox ({ index, moduleId }) {
         fetchModuleInfo();
     }, [])
 
-    if (!moduleInfo) {
-        return null;
-    } else {
-        return (
-            <div className="flex sm:flex-row flex-col justify-center w-full py-5 px-6 hover:bg-gray-300 hover:shadow-lg duration-300 cursor-pointer" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
-                <div className="flex justify-center items-center w-full sm:w-2/10 h-24">
-                    {moduleInfo && <Thumbnail thumbnailImageId={moduleInfo.module_image_path} />}
-                    {!moduleInfo && <div className="bg-gray-400 w-full h-full"></div>}
-                </div>
-                <div className="flex flex-row sm:flex-col justify-center w-full sm:w-7/10 sm:pl-6">
-                    
-                    <div className="flex flex-col w-9/10 pr-6 ">
-                        <div className="flex w-full">
-                            {moduleInfo && <p className="text-left font-bold">
-                                Módulo {index + 1}: {moduleInfo.module_title}
-                            </p>}
-                            {!moduleInfo && <div className="bg-gray-400 w-5/10 h-16"></div>}
-                        </div>
-                        <div className="flex w-full">
-                            {moduleInfo && <p className="text-left opacity-70">
-                                {moduleInfo.module_description.slice(0,80)}...
-                            </p>}
-                            {!moduleInfo && <div className="bg-gray-400 w-8/10 h-16"></div>}
-                        </div>
-                    </div>
-
-                    <div className="sm:hidden flex justify-end items-center w-1/10 mt-1 ">
-                        <button className="text-black hover:text-gray-100 duration-300">
-                            <MoreVertIcon />
-                        </button>
-                    </div>
-
-                </div>
-                <div className="hidden sm:flex justify-end items-center w-1/10 ">
-                    <button className="text-black hover:text-gray-100 duration-300">
-                        <MoreVertIcon />
-                    </button>
-                </div>
+    return (
+        <div className="flex sm:flex-row flex-col justify-center w-full py-5 px-6 hover:bg-gray-300 hover:shadow-lg duration-300 cursor-pointer" onClick={() => router.push("/courses/" + moduleInfo.course_id + "/" + moduleInfo.module_id)} key={index}>
+            <div className="flex justify-center items-center w-full sm:w-2/10 h-24">
+                {moduleInfo && <Thumbnail thumbnailImageId={moduleInfo.module_image_path} />}
+                {!moduleInfo && <div className="bg-gray-400 w-full h-full rounded-md"></div>}
             </div>
-        )
-    }
+            <div className="flex flex-row sm:flex-col justify-center w-full sm:w-7/10 sm:pl-6">
+                
+                <div className="flex flex-col w-9/10 pr-6 ">
+                    <div className="flex w-full">
+                        {moduleInfo && <p className="text-left font-bold">Módulo {index + 1}: {moduleInfo.module_title}</p>}
+                        {!moduleInfo && <div className="bg-gray-800 w-5/10 h-5 rounded-2xl mt-1"></div>}
+                    </div>
+                    <div className="flex w-full">
+                        {moduleInfo && <p className="text-left opacity-70">{moduleInfo.module_description.slice(0,80)}...</p>}
+                        {!moduleInfo && <div className="bg-gray-400 w-9/10 h-5 rounded-2xl mt-1"></div>}
+                    </div>
+                </div>
+
+                <div className="sm:hidden flex justify-end items-center w-1/10 mt-1 ">
+                    {moduleInfo && <button className="text-black hover:text-gray-100 duration-300"><MoreVertIcon /></button>}
+                </div>
+
+            </div>
+            <div className="hidden sm:flex justify-end items-center w-1/10 ">
+                {moduleInfo && <button className="text-black hover:text-gray-100 duration-300"><MoreVertIcon /></button>}
+            </div>
+        </div>
+    )
 }

32406d75e0a9896b34499a587f5a79c928736cb1
Manuel Alanis <manuelaag99@gmail.com>
2023-12-08 23:06:09 -0600

    functional admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,13 @@
+"use client"
+import TopNavigationBar from "../Components/TopNavigationBar";
+
 export default function AdminPage () {
     return (
         <div>
-            
+            <TopNavigationBar />
+            <div className="flex flex-col sm:w-85percent w-95percent mx-auto justify-center b-white rounded-md sm:mt-40 mt-20 mb-28 pb-16 shadow-2xl">
+
+            </div>
         </div>
     )
 }

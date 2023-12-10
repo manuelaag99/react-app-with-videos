9b4c5807ee366af0d6bbef54e6f623b7f03af133
Manuel Alanis <manuelaag99@gmail.com>
2023-12-09 19:04:57 -0600

    add details to admin page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,11 +1,25 @@
 "use client"
+import { useState } from "react";
+import Button from "../Components/Button";
 import TopNavigationBar from "../Components/TopNavigationBar";
 
 export default function AdminPage () {
+
+    const [openCreatePortal, setOpenCreatePortal] = useState();
     return (
         <div>
             <TopNavigationBar />
-            <div className="flex flex-col sm:w-85percent w-95percent mx-auto justify-center b-white rounded-md sm:mt-40 mt-20 mb-28 pb-16 shadow-2xl">
+            <div className="flex flex-col sm:w-85percent w-95percent mx-auto justify-center bg-white rounded-md sm:mt-40 mt-20 mb-28 pb-16 pt-7 px-7 shadow-2xl">
+
+                <div className="flex justify-center w-9/10 mx-auto -mt-6">
+                    <p className="text-center text-black font-bold font-amatic text-page-title-desktop">
+                        Cursos
+                    </p>
+                </div>
+                <div className="flex flex-row justify-around w-9/10 mb-4 mx-auto mt-4">
+                    <Button contentForButton="Crear un curso" additionalClassNamesForButton=" w-3/10 bg-var-2 mx-auto " additionalClassNamesForText=" text-button-desktop text-white font-amatic font-bold" onClickButtonAction={() => setOpenCreatePortal(true)}  />
+                    <Button contentForButton="Crear un módulo" additionalClassNamesForButton=" w-3/10 bg-var-2 mx-auto " additionalClassNamesForText=" text-button-desktop text-white font-amatic font-bold" onClickButtonAction={() => setOpenCreatePortal(true)}  />
+                </div>
 
             </div>
         </div>

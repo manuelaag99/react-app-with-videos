33016f90c7577dd67ae92e7d5c5fbbf61bda0a58
Manuel Alanis <manuelaag99@gmail.com>
2023-12-10 19:29:21 -0600

    add upload fields to 'create content' popup

--- src/app/Components/Portals/CreateContentPopUp.jsx
+++ src/app/Components/Portals/CreateContentPopUp.jsx
@@ -4,9 +4,9 @@ import VideoUpload from "../VideoUpload";
 
 export default function CreateContentPopUp ({ content, onClose, open }) {
     const createContentPopUp = (
-        <div>
+        <div className="h-full">
             <div className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-30" onClick={onClose}></div>
-            <div className="flex flex-col justify-center fixed w-9/10 sm:w-6/10 h-6/10 bg-white rounded-md shadow-2xl left-[5%] sm:left-[20%] top-[10%] z-40 p-4 sm:p-9 ">
+            <div className="flex flex-col justify-center fixed w-9/10 sm:w-6/10 h-fit bg-white rounded-md shadow-2xl left-[5%] sm:left-[20%] top-[10%] z-40 p-4 sm:p-9 ">
                 <div className="flex justify-center mx-auto w-95percent ">
                     <p className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">
                         {(content === "course") && "Crear curso"}
@@ -26,22 +26,26 @@ export default function CreateContentPopUp ({ content, onClose, open }) {
 
                 </div>
 
-                <div className="flex flex-row justify-between w-95percent mx-auto">
-                    {(content === "course") && <div className="flex w-46percent">
-                        <VideoUpload />
-                    </div>}
-                    {(content === "course") && <div className="flex w-46percent">
-                        <VideoUpload />
-                    </div>}
-                    {(content === "module") && <div className="flex w-46percent">
-                        <VideoUpload />
-                    </div>}
-                    {(content === "module") && <div className="flex w-46percent">
-                        <VideoUpload />
-                    </div>}
+                <div className="flex flex-col w-95percent mx-auto">
+                    {(content === "course") && <label className="text-sign-in-or-sign-up-labels-desktop font-bold w-full text-left" htmlFor="">Si vas a crear un curso, se recomienda agregar un video con un pequeño fragmento de algún modulo. Asimismo, para que el curso tenga una miniatura, puedes subir una imagen, de dimensiones 16:9.</label>}
+                    {(content === "module") && <label className="text-sign-in-or-sign-up-labels-desktop font-bold w-full text-left" htmlFor="">Si vas a crear un módulo, tienes que subir a continuación el video corespondiente, así como una miniatura del mismo, de dimensiones 16:9.</label>}
+                    <div className="flex flex-row justify-between w-full mx-auto h-28 mb-3">
+                        {(content === "course") && <div className="flex w-46percent h-full">
+                            <VideoUpload additionalClassnames=" h-full" instructionForUpload="Miniatura para el curso" typeOfFile="image" />
+                        </div>}
+                        {(content === "course") && <div className="flex w-46percent">
+                            <VideoUpload additionalClassnames=" h-full" instructionForUpload="Video de fragmento del curso" typeOfFile="video" />
+                        </div>}
+                        {(content === "module") && <div className="flex w-46percent">
+                            <VideoUpload additionalClassnames=" h-full" instructionForUpload="Miniatura para el módulo" typeOfFile="image" />
+                        </div>}
+                        {(content === "module") && <div className="flex w-46percent">
+                            <VideoUpload additionalClassnames=" h-full" instructionForUpload="Video correspondiente al módulo" typeOfFile="video" />
+                        </div>}
+                    </div>
                 </div>
 
-                <Button additionalClassNamesForButton=" w-95percent py-3 bg-var-2 hover:bg-var-2-hovered duration-200 mx-auto rounded-sm mt-3" additionalClassNamesForText=" text-white text-button-desktop font-amatic font-bold" contentForButton="Crear" onClickButtonAction={() => console.log("click")} />
+                <Button additionalClassNamesForButton=" w-95percent py-3 bg-var-2 hover:bg-var-2-hovered duration-200 mx-auto rounded-sm my-3" additionalClassNamesForText=" text-white text-button-desktop font-amatic font-bold" contentForButton="Crear" onClickButtonAction={() => console.log("click")} />
             </div>
         </div>
     );
--- src/app/Components/VideoUpload.jsx
+++ src/app/Components/VideoUpload.jsx
@@ -1,25 +1,63 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import Videocam from "@mui/icons-material/Videocam";
 
-export default function VideoUpload ({}) {
+export default function VideoUpload ({ additionalClassnames, instructionForUpload, typeOfFile }) {
+    const imageSelectorRef = useRef();
     const [file, setFile] = useState();
-    const [previewVideo, setPreviewVideo] = useState();
+    const [preview, setPreview] = useState();
 
     useEffect(() => {
         if (!file) return;
         const fileReader = new FileReader();
-        fileReader.onload = () => setPreviewVideo(fileReader.result);
+        fileReader.onload = () => setPreview(fileReader.result);
         fileReader.readAsDataURL(file);
     }, [file])
 
+    function cancelImageUpload () {
+        if (file) {
+            setFile();
+            setPreview();
+        } else {
+            setPreview();
+        }
+    }
+
     function uploadImage (e) {
-        setPreviewVideo(e.target.file[0])
+        setPreview(e.target.file[0])
         setFile(e.target.file[0]);
     }
 
+    function selectFileHandler (e) {
+        imageSelectorRef.current.click(e);
+    }
+
+
+
     return (
-        <div className="flex justify-center w-full my-5 ">
-            <input onChange={(e) => uploadImage(e)} type="file" />
+        <div className={"flex justify-center w-full " + additionalClassnames}>
+            <div className="flex justify-center w-full h-full relative aspect-square">
+                {/* {preview && isPostPhoto && <div className="absolute top-0 right-0 m-2 z-20">
+                    <button className="bg-black hover:bg-gray-600 duration-200 rounded-post px-2 text-white mr-3" onClick={(e) => selectFileHandler(e)}>
+                        change
+                    </button>
+                    <button className="bg-black hover:bg-gray-600 duration-200 rounded-post px-2 text-white" onClick={cancelImageUpload}>
+                        <CloseIcon />
+                    </button>
+                </div>} */}
+                {!file && !preview && <button className="flex justify-center items-center bg-gray-200 hover:bg-gray-300 duration-200 w-full h-full z-10 cursor-pointer rounded-sm " onClick={(e) => selectFileHandler(e)}>
+                    {(typeOfFile === "image") && <p className="text-gray-600 text-center cursor-pointer z-20 w-85percent mx-auto " onClick={(e) => selectFileHandler(e)}>
+                        <InsertPhotoIcon />  {instructionForUpload}
+                    </p>}
+                    {(typeOfFile === "video") && <p className="text-gray-600 text-center cursor-pointer z-20 w-85percent mx-auto " onClick={(e) => selectFileHandler(e)}>
+                        <Videocam />  {instructionForUpload}
+                    </p>}
+                </button>}
+                {preview && <img alt="image" className="object-contain aspect-square w-full z-10 cursor-pointer  " src={preview}  />}
+                <input className="w-9/10 text-center hidden" onChange={(e) => uploadImage(e)} ref={imageSelectorRef} type="file" />
+            </div>
         </div>
     )
 }

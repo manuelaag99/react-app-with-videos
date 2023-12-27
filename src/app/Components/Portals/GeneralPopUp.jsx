ca24a3c0c4edfb0d3e41185feadba65c244b1daa
Manuel Alanis <manuelaag99@gmail.com>
2023-12-26 20:35:46 -0600

    add content to general pop up

--- src/app/Components/Portals/GeneralPopUp.jsx
+++ src/app/Components/Portals/GeneralPopUp.jsx
@@ -1,10 +1,22 @@
 import { createPortal } from "react-dom"
 
-export default function GeneralPopUp ({}) {
+export default function GeneralPopUp ({ onClose, textForPopUp }) {
 
     const generalPopUp = (
         <div>
-
+            <div className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-30" onClick={onClose}></div>
+            <div className="flex flex-col justify-center fixed w-7/10 sm:w-5/10 h-fit bg-white rounded-md shadow-2xl left-[15%] sm:left-[25%] top-[25%] z-50 p-4 sm:p-9 ">
+                <div className="flex w-full px-6 justify-center items-center h-14">
+                </div>
+                <div className="flex flex-col w-full px-6 justify-center items-center h-fit">
+                    <div className="flex w-full">
+                        <p>{textForPopUp}</p>
+                    </div>
+                    <div className="flex w-full justify-between">
+                    
+                    </div>
+                </div>
+            </div>
         </div>
     )
 

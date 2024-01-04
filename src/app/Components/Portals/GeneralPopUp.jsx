11013e8d3d79b5ad456fb34275460daf5791fece
Manuel Alanis <manuelaag99@gmail.com>
2024-01-03 21:08:16 -0600

    add buttons to general popup

--- src/app/Components/Portals/GeneralPopUp.jsx
+++ src/app/Components/Portals/GeneralPopUp.jsx
@@ -1,19 +1,19 @@
 import { createPortal } from "react-dom"
+import Button from "../Button";
 
-export default function GeneralPopUp ({ onClose, open, textForPopUp }) {
+export default function GeneralPopUp ({ onClose, open, textForButtonOne, textForButtonTwo, textForPopUp }) {
 
     const generalPopUp = (
         <div>
             <div className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-40" onClick={onClose}></div>
             <div className="flex flex-col justify-center fixed w-7/10 sm:w-5/10 h-fit bg-white rounded-md shadow-2xl left-[15%] sm:left-[25%] top-[25%] z-50 p-4 sm:p-9 ">
-                <div className="flex w-full px-6 justify-center items-center h-14">
-                </div>
                 <div className="flex flex-col w-full px-6 justify-center items-center h-fit">
                     <div className="flex w-full">
                         <p>{textForPopUp}</p>
                     </div>
                     <div className="flex w-full justify-between">
-                    
+                        <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" bg-var-1 text-white bg-var-1-hovered duration-200" contentForButton={textForButtonOne} onClickButtonAction={() => console.log("click")} />
+                        <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" bg-var-1 text-white bg-var-1-hovered duration-200" contentForButton={textForButtonTwo} onClickButtonAction={() => console.log("click")} />
                     </div>
                 </div>
             </div>

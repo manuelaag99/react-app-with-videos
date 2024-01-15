0cf85b4ffffffba9e5738bc8f43390cfa09ff06f
Manuel Alanis <manuelaag99@gmail.com>
2024-01-15 16:40:58 -0600

    format buttons in home

--- src/app/page.js
+++ src/app/page.js
@@ -39,12 +39,12 @@ export default function Home() {
 				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">Cursos de repostería</div>
 				<div></div>
 				<div>
-					<Button contentForButton="Iniciar Sesión" />
-					<Button contentForButton="Registrarse" />
+					<Button additionalClassNamesForButton=" bg-var-3 hover:bg-var-3-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic mr-5 " contentForButton="Iniciar Sesión" />
+					<Button additionalClassNamesForButton=" bg-var-2 hover:bg-var-2-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic " contentForButton="Registrarse" />
 				</div>
 			</div>
-			<div className="flex sm:w-4/10 w-full">
-				<Image additionalClassNames="flex justify-center items-center w-9/10 mx-auto" imageAlt="" imageSource="" />
+			<div className="flex sm:w-4/10 w-full p-4">
+				<img className="w-full mx-auto object-cover" src="" alt="" />
 			</div>
 
 		</div>
@@ -53,16 +53,12 @@ export default function Home() {
 		<div className="flex sm:flex-row flex-col w-full bg-var-4">
 			
 			<div className="flex sm:w-4/10 w-full">
-				<Image additionalClassNames="flex justify-center items-center w-full mx-auto" imageAlt="" imageSource="https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/385872138_706349364849248_1363954291548222754_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGSJ76EEgn0QI_H248vPkzzRCbmkLFax5lEJuaQsVrHmZXVcyWHWQ0LcUoetBof3lA&_nc_ohc=RHAlsn_V58MAX_3jZm_&_nc_oc=AQkO7RBlGhf0ARH1ZiRNDQDfzQ-R80RGhqCg7EyKMWPfPmx392a07EiDoEAucBBuhu03--EJWUJ5wMVSCBvzQo8E&_nc_ht=scontent.fntr5-1.fna&oh=00_AfCnmUVNplXlVCILP3hFDsGU_6uLdJkH8UHTN7jWm_zofw&oe=65AA8F3B" />
+				<img className="w-full mx-auto object-cover h-72" src="https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/385872138_706349364849248_1363954291548222754_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGSJ76EEgn0QI_H248vPkzzRCbmkLFax5lEJuaQsVrHmZXVcyWHWQ0LcUoetBof3lA&_nc_ohc=RHAlsn_V58MAX_3jZm_&_nc_oc=AQkO7RBlGhf0ARH1ZiRNDQDfzQ-R80RGhqCg7EyKMWPfPmx392a07EiDoEAucBBuhu03--EJWUJ5wMVSCBvzQo8E&_nc_ht=scontent.fntr5-1.fna&oh=00_AfCnmUVNplXlVCILP3hFDsGU_6uLdJkH8UHTN7jWm_zofw&oe=65AA8F3B" alt="" />
 			</div>
 
 			<div className="flex flex-col sm:w-6/10 w-full px-6 py-9">
-				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">Cursos de repostería</div>
+				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">Hacer un pedido</div>
 				<div></div>
-				<div>
-					<Button contentForButton="Iniciar Sesión" />
-					<Button contentForButton="Registrarse" />
-				</div>
 			</div>
 
 		</div>

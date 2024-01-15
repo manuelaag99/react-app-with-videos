e2188c697ae9368c4b32511ae5a71ad0688f08c4
Manuel Alanis <manuelaag99@gmail.com>
2024-01-15 15:38:44 -0600

    add formatting to home sections

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import TopNavigationBar from "./Components/TopNavigationBar";
 import { supabase } from "./supabase/client";
 import Button from "./Components/Button";
+import Image from "./Components/Image";
 
 export default function Home() {
 
@@ -35,7 +36,7 @@ export default function Home() {
 
 		<div className="flex sm:flex-row flex-col w-full bg-white px-6 py-9">
 			<div className="flex flex-col sm:w-6/10 w-full">
-				<div>Cursos de repostería</div>
+				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">Cursos de repostería</div>
 				<div></div>
 				<div>
 					<Button contentForButton="Iniciar Sesión" />
@@ -43,20 +44,20 @@ export default function Home() {
 				</div>
 			</div>
 			<div className="flex sm:w-4/10 w-full">
-				<img src="" alt="" />
+				<Image additionalClassNames="flex justify-center items-center w-9/10 mx-auto" imageAlt="" imageSource="" />
 			</div>
 
 		</div>
 
 
-		<div className="flex sm:flex-row flex-col w-full bg-white px-6 py-9">
+		<div className="flex sm:flex-row flex-col w-full bg-white">
 			
 			<div className="flex sm:w-4/10 w-full">
-				<img src="" alt="" />
+				<Image additionalClassNames="flex justify-center items-center w-full mx-auto" imageAlt="" imageSource="https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/385872138_706349364849248_1363954291548222754_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGSJ76EEgn0QI_H248vPkzzRCbmkLFax5lEJuaQsVrHmZXVcyWHWQ0LcUoetBof3lA&_nc_ohc=RHAlsn_V58MAX_3jZm_&_nc_oc=AQkO7RBlGhf0ARH1ZiRNDQDfzQ-R80RGhqCg7EyKMWPfPmx392a07EiDoEAucBBuhu03--EJWUJ5wMVSCBvzQo8E&_nc_ht=scontent.fntr5-1.fna&oh=00_AfCnmUVNplXlVCILP3hFDsGU_6uLdJkH8UHTN7jWm_zofw&oe=65AA8F3B" />
 			</div>
 
-			<div className="flex flex-col sm:w-6/10 w-full">
-				<div>Cursos de repostería</div>
+			<div className="flex flex-col sm:w-6/10 w-full px-6 py-9">
+				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop">Cursos de repostería</div>
 				<div></div>
 				<div>
 					<Button contentForButton="Iniciar Sesión" />

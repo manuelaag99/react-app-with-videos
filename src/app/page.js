57fb832603c4aa714e779f142e6c48d444aea602
Manuel Alanis <manuelaag99@gmail.com>
2024-01-16 13:08:57 -0600

    add text and button to order section in home

--- src/app/page.js
+++ src/app/page.js
@@ -50,18 +50,18 @@ export default function Home() {
 		<div className="flex w-full h-64 relative">
 			{homePhoto && <img className="w-full object-cover" src={homePhoto} alt="home-photo" />}
 			<div className="absolute flex w-full mx-auto justify-center items-center">
-				{caiLogo && <img className="sm:w-5/10 mt-10" src={caiLogo} alt="cai-logo" />}
+				{caiLogo && <img className="sm:w-3/10 mt-14" src={caiLogo} alt="cai-logo" />}
 			</div>
 			
 		</div>
 
 		<div className="flex sm:flex-row flex-col w-full bg-white px-6 py-9 shadow-lg">
 			<div className="flex flex-col sm:w-6/10 w-full pr-6">
-				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop -mt-2">Cursos de repostería</div>
-				<div></div>
+				<div className="font-amatic font-bold text-page-title-desktop -mt-4">Cursos de repostería</div>
+				<div className="mb-5 mt-2 w-full">Breve explicación sobre los cursos de repostería aquí.</div>
 				<div>
-					<Button additionalClassNamesForButton=" bg-var-3 hover:bg-var-3-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic mr-5 " contentForButton="Iniciar Sesión" />
-					<Button additionalClassNamesForButton=" bg-var-2 hover:bg-var-2-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic " contentForButton="Registrarse" />
+					<Button additionalClassNamesForButton=" px-7 shadow-md bg-var-3 hover:bg-var-3-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic font-bold mr-5 " contentForButton="Iniciar Sesión" />
+					<Button additionalClassNamesForButton=" px-7 shadow-md bg-var-2 hover:bg-var-2-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic font-bold " contentForButton="Registrarse" />
 				</div>
 			</div>
 			<div className="flex sm:w-4/10 w-full">
@@ -71,15 +71,18 @@ export default function Home() {
 		</div>
 
 
-		<div className="flex sm:flex-row flex-col w-full bg-var-4 shadow-lg mb-10">
+		<div className="flex sm:flex-row flex-col w-full bg-var-4 shadow-lg mb-10 z-">
 			
 			<div className="flex sm:w-4/10 w-full">
 				<img className="w-full mx-auto object-cover h-72" src="https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/385872138_706349364849248_1363954291548222754_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGSJ76EEgn0QI_H248vPkzzRCbmkLFax5lEJuaQsVrHmZXVcyWHWQ0LcUoetBof3lA&_nc_ohc=RHAlsn_V58MAX_3jZm_&_nc_oc=AQkO7RBlGhf0ARH1ZiRNDQDfzQ-R80RGhqCg7EyKMWPfPmx392a07EiDoEAucBBuhu03--EJWUJ5wMVSCBvzQo8E&_nc_ht=scontent.fntr5-1.fna&oh=00_AfCnmUVNplXlVCILP3hFDsGU_6uLdJkH8UHTN7jWm_zofw&oe=65AA8F3B" alt="" />
 			</div>
 
 			<div className="flex flex-col sm:w-6/10 w-full px-6 py-9">
-				<div className="font-amatic font-bold text-sign-in-or-sign-up-title-desktop -mt-2">Hacer un pedido</div>
-				<div></div>
+				<div className="font-amatic font-bold text-page-title-desktop -mt-4">Hacer un pedido</div>
+				<div className="mb-5 mt-2 w-full">Breve explicación sobre los pedidos.</div>
+				<div>
+					<Button additionalClassNamesForButton=" px-7 shadow-md bg-var-2 hover:bg-var-2-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic font-bold mr-5 " contentForButton="Ver productos" />
+				</div>
 			</div>
 
 		</div>

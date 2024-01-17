e91e9c0bb0e796133f1438f200c3a2e755f4b9e1
Manuel Alanis <manuelaag99@gmail.com>
2024-01-17 09:59:12 -0600

    create list component

---
+++ src/app/Components/ItemList.jsx
@@ -0,0 +1,26 @@
+export default function ItemList ({}) {
+    return (
+        <div className="flex flex-col w-full">
+            <div className="flex w-full mt-2 mb-1 justify-center items-center">
+                <p className="text-center font-bold font-rubik">Lista de cursos</p>
+            </div>
+            <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
+                <div className="flex w-full h-fit">
+                    <div className="flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer">
+                        <div className="flex w-full sm:w-8/10">
+                            Nombre de artículo
+                        </div>
+                        <div className="flex flex-row w-2/10">
+                            <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                <EditIcon color="black" />
+                            </button>
+                            <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                <DeleteIcon color="black" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

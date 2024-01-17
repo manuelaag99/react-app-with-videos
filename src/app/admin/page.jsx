1664d0bdfea33bbb22df15f492d63fb55bd0c487
Manuel Alanis <manuelaag99@gmail.com>
2024-01-16 20:54:03 -0600

    add lists to admin page sections

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -23,7 +23,7 @@ export default function AdminPage () {
     return (
         <div>
             <TopNavigationBar />
-            <div className="flex flex-col sm:flex-row sm:w-85percent w-95percent mx-auto justify-center bg-white rounded-md sm:mt-40 mt-20 mb-28 pb-16 pt-7 px-7 shadow-2xl">
+            <div className="flex flex-col sm:flex-row sm:w-85percent w-95percent mx-auto justify-center items-start bg-white rounded-md sm:mt-40 mt-20 mb-28 pb-20 pt-7 px-7 shadow-2xl">
 
                 <div className="flex flex-col justify-center sm:w-1/2 w-full">
                     <div className="flex justify-center w-full mx-auto ">
@@ -39,18 +39,20 @@ export default function AdminPage () {
                     <div className="flex w-full mt-2 mb-1 justify-center items-center">
                         <p className="text-center font-bold font-rubik">Lista de cursos</p>
                     </div>
-                    <div className="flex justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik">
-                        <div className="flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer">
-                            <div className="flex w-full sm:w-8/10">
-                                Nombre de artículo
-                            </div>
-                            <div className="flex flex-row w-2/10">
-                                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
-                                    <EditIcon color="black" />
-                                </button>
-                                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
-                                    <DeleteIcon color="black" />
-                                </button>
+                    <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
+                        <div className="flex w-full h-fit">
+                            <div className="flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer">
+                                <div className="flex w-full sm:w-8/10">
+                                    Nombre de artículo
+                                </div>
+                                <div className="flex flex-row w-2/10">
+                                    <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                        <EditIcon color="black" />
+                                    </button>
+                                    <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                        <DeleteIcon color="black" />
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -70,18 +72,20 @@ export default function AdminPage () {
                     <div className="flex w-full mt-2 mb-1 justify-center items-center">
                         <p className="text-center font-bold font-rubik">Lista de productos</p>
                     </div>
-                    <div className="flex justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik">
-                        <div className="flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer">
-                            <div className="flex w-full sm:w-8/10">
-                                Nombre de artículo
-                            </div>
-                            <div className="flex flex-row w-2/10">
-                                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
-                                    <EditIcon color="black" />
-                                </button>
-                                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
-                                    <DeleteIcon color="black" />
-                                </button>
+                    <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
+                        <div className="flex w-full h-fit">
+                            <div className="flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer">
+                                <div className="flex w-full sm:w-8/10">
+                                    Nombre de artículo
+                                </div>
+                                <div className="flex flex-row w-2/10">
+                                    <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                        <EditIcon color="black" />
+                                    </button>
+                                    <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                                        <DeleteIcon color="black" />
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>

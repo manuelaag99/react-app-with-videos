a8367eaf4b57660483f29f4c91835c9a5bdad001
Manuel Alanis <manuelaag99@gmail.com>
2024-01-20 11:31:39 -0600

    add modules list to admin lists

--- src/app/Components/ItemList.jsx
+++ src/app/Components/ItemList.jsx
@@ -41,23 +41,31 @@ export default function ItemList ({ listCategory, listTitle }) {
 
         <div className="flex flex-col w-full mb-4">
             {!itemsForList && <p className="text-center font-bold mt-2">No hay nada que mostrar.</p>}
-            
             {itemsForList && (itemsForList.length > 0) && <div className="flex flex-col w-full">
                 <div className="flex w-full mt-2 mb-1 justify-center items-center">
                     <p className="text-center font-bold font-rubik">{listTitle}</p>
                 </div>
                 <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
                     <div className="flex flex-col w-full h-fit">
+                        
                         {itemsForList && (itemsForList.length > 0) && itemsForList.map((item, index) => {
                             return (
-                                <ListElement element={item} elementClassNames="" elementType={listCategory} key={index} index={index} onClickFunction={toggleSmallListVisibility} />
+                                <div className="flex flex-col w-full">
+                                    <ListElement element={item} elementClassNames="" elementType={listCategory} key={index} index={index} onClickFunction={toggleSmallListVisibility} />
+                                    {(listCategory === "courses") && <div className="flex flex-col w-full">
+                                        {modules.map((module, index) => {
+                                            if (module.course_id === item.id) {
+                                                return (
+                                                    <ListElement element={module} elementClassNames=" pl-6" elementId="" elementName="Nombre de artículo" key={index} />
+                                                )
+                                            }
+                                        })}
+                                    </div>}
+                                </div>
+                                
                             )
                         })}
-                        {listCategory === "courses" && isSmallListOpen && <div className="flex flex-col w-full">
-                            <div className="flex flex-col w-full">
-                                <ListElement elementClassNames=" pl-6" elementId="" elementName="Nombre de artículo" />
-                            </div>
-                        </div>}
+                        
                     </div>
                 </div>
             </div>}

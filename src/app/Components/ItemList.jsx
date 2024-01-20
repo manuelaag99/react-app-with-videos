bb4a71daa0312f6f8583376f19245201815b4963
Manuel Alanis <manuelaag99@gmail.com>
2024-01-19 20:35:08 -0600

    conditional rendering of itemlist on admin

--- src/app/Components/ItemList.jsx
+++ src/app/Components/ItemList.jsx
@@ -1,29 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ListElement from "./ListElement";
+import { supabase } from "../supabase/client";
 
 export default function ItemList ({ listCategory, listTitle }) {
     const [isSmallListOpen, setIsSmallListOpen] = useState(false);
 
+    const [itemsToList, setItemsToList] = useState(listCategory);
+    const [itemsForList, setItemsForList] = useState();
+    async function fetchItems () {
+        try {
+            const { data, error } = await supabase.from("cai-" + listCategory).select();
+            if (error) console.log (error);
+            setItemsForList(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchItems();
+    }, [])
 
+    console.log(itemsForList)
 
     function toggleSmallListVisibility (prevValue) {
         setIsSmallListOpen(prevValue => !prevValue)
     }
     return (
+
         <div className="flex flex-col w-full mb-4">
-            <div className="flex w-full mt-2 mb-1 justify-center items-center">
-                <p className="text-center font-bold font-rubik">{listTitle}</p>
-            </div>
-            <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
-                <div className="flex flex-col w-full h-fit">
-                    <ListElement elementClassNames="" elementId="" elementName="Nombre de artículo" onClickFunction={toggleSmallListVisibility} />
-                    {listCategory === "courses" && isSmallListOpen && <div className="flex flex-col w-full">
-                        <div className="flex flex-col w-full">
-                            <ListElement elementClassNames=" pl-6" elementId="" elementName="Nombre de artículo" />
-                        </div>
-                    </div>}
+            {!itemsForList && <p className="text-center font-bold mt-2">No hay nada que mostrar.</p>}
+            
+            {itemsForList && (itemsForList.length > 0) && <div className="flex flex-col w-full">
+                <div className="flex w-full mt-2 mb-1 justify-center items-center">
+                    <p className="text-center font-bold font-rubik">{listTitle}</p>
                 </div>
-            </div>
+                <div className="flex flex-col justify-center w-9/10 overflow-y-scroll my-2 mx-auto font-rubik max-h-64">
+                    <div className="flex flex-col w-full h-fit">
+                        <ListElement elementClassNames="" elementId="" elementName="Nombre de artículo" onClickFunction={toggleSmallListVisibility} />
+                        {listCategory === "courses" && isSmallListOpen && <div className="flex flex-col w-full">
+                            <div className="flex flex-col w-full">
+                                <ListElement elementClassNames=" pl-6" elementId="" elementName="Nombre de artículo" />
+                            </div>
+                        </div>}
+                    </div>
+                </div>
+            </div>}
+
         </div>
     )
 }

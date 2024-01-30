5a12883569ab8ba8e1d9006a25acae65e48eaf2e
Manuel Alanis <manuelaag99@gmail.com>
2024-01-30 10:24:15 -0600

    conditional rendering of buttons on top nav bar

--- src/app/Components/TopNavigationBar.jsx
+++ src/app/Components/TopNavigationBar.jsx
@@ -1,11 +1,13 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { supabase } from '../supabase/client';
 import Link from 'next/link';
 import MobileMenu from './Portals/MobileMenu';
 import SignInOrSignUpPopUp from './Portals/SignInOrSignUpPopUp';
+import { AuthContext } from '../context/AuthContext';
 
 export default function TopNavigationBar ({}) {
+    const auth = useContext(AuthContext);
     const [caiLogo, setCaiLogo] = useState();
     async function fetchLogo () {
         try {
@@ -16,10 +18,23 @@ export default function TopNavigationBar ({}) {
             console.log (err);
         }
     }
+    
+    const [admins, setAdmins] = useState()
+    async function fetchAdmins () {
+        try {
+            const { data, error } = await supabase.from("cai-admins").select().eq("user_id", auth.uId);
+            if (error) console.log(error);
+            if (data) setAdmins(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
     useEffect(() => {
         fetchLogo();
+        fetchAdmins();
     }, [])
 
+    console.log(admins)
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState();
 
     const [openSignInOrSignUpWindow, setOpenSignInOrSignUpWindow] = useState();
@@ -56,26 +71,26 @@ export default function TopNavigationBar ({}) {
                             Cursos
                         </p>
                     </Link>
-                    <div className="px-3 cursor-pointer pt-2" onClick={openSignUpWindow}>
+                    {!auth.isLoggedIn && <div className="px-3 cursor-pointer pt-2" onClick={openSignUpWindow}>
                         <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap mt-1">
                             Registrarse
                         </p>
-                    </div>
-                    <div className="px-3 cursor-pointer pt-2" onClick={openSignInWindow}>
+                    </div>}
+                    {!auth.isLoggedIn && <div className="px-3 cursor-pointer pt-2" onClick={openSignInWindow}>
                         <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap mt-1">
                             Iniciar sesión
                         </p>
-                    </div>
-                    <Link className="px-3 cursor-pointer pt-2" href="/admin/">
+                    </div>}
+                    {admins && (admins.length > 0) && admins.includes(auth.uId) && <Link className="px-3 cursor-pointer pt-2" href="/admin/">
                         <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap mt-1">
                             Administrar
                         </p>
-                    </Link>
-                    <div className="px-3 cursor-pointer pt-2">
+                    </Link>}
+                    {auth.isLoggedIn && <div className="px-3 cursor-pointer pt-2">
                         <p className="text-white hover:text-var-1-hovered duration-200 whitespace-nowrap mt-1">
                             Cerrar sesión
                         </p>
-                    </div>
+                    </div>}
                 </div>
             </div>
             <MobileMenu onClose={() => setIsMobileMenuOpen(false)} open={isMobileMenuOpen} />

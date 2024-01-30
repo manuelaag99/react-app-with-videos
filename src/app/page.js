4828b5e7fbf73f27b1624bd7530c0f7e3d715865
Manuel Alanis <manuelaag99@gmail.com>
2024-01-29 19:02:57 -0600

    add customized z index values and fix button justify content

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
 		
 
 
-		<div className="flex w-full h-64 relative">
+		<div className="flex w-full h-64 z-4">
 			{homePhoto && <img className="w-full object-cover" src={homePhoto} alt="home-photo" />}
 			<div className="absolute flex w-full mx-auto justify-center items-center">
 				{caiLogo && <img className="sm:w-3/10 mt-14" src={caiLogo} alt="cai-logo" />}
@@ -57,11 +57,11 @@ export default function Home() {
 			
 		</div>
 
-		<div className="flex sm:flex-row flex-col-reverse w-full bg-white px-6 py-9 shadow-lg">
+		<div className="flex sm:flex-row flex-col-reverse w-full bg-white px-6 py-9 shadow-lg z-5">
 			<div className="flex flex-col sm:w-6/10 w-full sm:pr-6">
 				<div className="font-amatic font-bold text-page-title-desktop sm:-mt-4 mt-0">Cursos de repostería</div>
 				<div className="mb-5 mt-2 w-full">Breve explicación sobre los cursos de repostería aquí.</div>
-				<div className="flex flex-col sm:flex-row justify-center">
+				<div className="flex flex-col sm:flex-row justify-center sm:justify-start">
 					<Button additionalClassNamesForButton=" flex w-full sm:w-fit justify-center items-center px-7 shadow-md bg-var-3 hover:bg-var-3-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic font-bold mb-4 sm:mb-0 sm:mr-5 " additionalClassNamesForText=" text-center" contentForButton="Iniciar Sesión" />
 					<Button additionalClassNamesForButton=" flex w-full sm:w-fit justify-center items-center px-7 shadow-md bg-var-2 hover:bg-var-2-hovered duration-200 cursor-pointer text-white text-button-desktop rounded-md font-amatic font-bold " additionalClassNamesForText=" text-center" contentForButton="Registrarse" />
 				</div>
@@ -74,7 +74,7 @@ export default function Home() {
 		</div>
 
 
-		<div className="flex sm:flex-row flex-col w-full bg-var-4 shadow-lg mb-10 z-">
+		<div className="flex sm:flex-row flex-col w-full bg-var-4 shadow-lg mb-10 z-6">
 			
 			<div className="flex sm:w-4/10 w-full">
 				{productsPhoto && <img className="w-full mx-auto object-cover h-44 sm:h-72" src={productsPhoto} alt="" />}

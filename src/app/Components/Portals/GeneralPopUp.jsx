76bfaa95d81bc295d4b56093facb898bdefa06b1
Manuel Alanis <manuelaag99@gmail.com>
2024-02-01 20:16:52 -0600

    fix general popup styling

--- src/app/Components/Portals/GeneralPopUp.jsx
+++ src/app/Components/Portals/GeneralPopUp.jsx
@@ -4,15 +4,15 @@ import Button from "../Button";
 export default function GeneralPopUp ({ infoForPopUp, onClose, open }) {
     const generalPopUp = (
         <div>
-            <div className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-40" onClick={onClose}></div>
-            <div className="flex flex-col justify-center fixed w-7/10 sm:w-5/10 h-fit bg-white rounded-md shadow-2xl left-[15%] sm:left-[25%] top-[25%] z-50 p-4 sm:p-9 ">
+            <div className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-80" onClick={onClose}></div>
+            <div className="flex flex-col justify-center fixed w-7/10 sm:w-5/10 h-fit bg-white rounded-md shadow-2xl left-[15%] sm:left-[25%] top-[25%] z-90 px-4 py-6 sm:px-9 ">
                 <div className="flex flex-col w-full px-6 justify-center items-center h-fit">
                     <div className="flex w-full mb-4 justify-center">
                         <p className="text-center">{infoForPopUp.message}</p>
                     </div>
-                    <div className="flex flex-col sm:flex-row w-full justify-between">
-                        {(infoForPopUp !== "") && <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" bg-var-1 text-white bg-var-1-hovered duration-200 rounded-md shadow-md sm:mb-0 mb-4" contentForButton={infoForPopUp.textForButtonOne} onClickButtonAction={onClose} />}
-                        {(infoForPopUp !== "") && <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" bg-var-1 text-white bg-var-1-hovered duration-200 rounded-md shadow-md sm:mb-0" contentForButton={infoForPopUp.textForButtonTwo} onClickButtonAction={() => console.log("click")} />}
+                    <div className={"flex flex-col sm:flex-row w-full " + ((infoForPopUp.textForButtonTwo !== "") ? " justify-between" : " justify-center" )}>
+                        {(infoForPopUp.textForButtonOne !== "") && <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" flex justify-center py-2 bg-var-1 text-white hover:bg-var-1-hovered duration-200 rounded-md shadow-md sm:mt-0" contentForButton={infoForPopUp.textForButtonOne} onClickButtonAction={onClose} />}
+                        {(infoForPopUp.textForButtonTwo !== "") && <Button additionalClassNamesForText=" text-center" additionalClassNamesForButton=" flex justify-center py-2 bg-var-1 text-white hover:bg-var-1-hovered duration-200 rounded-md shadow-md sm:mt-0 mt-4" contentForButton={infoForPopUp.textForButtonTwo} onClickButtonAction={() => console.log("click")} />}
                     </div>
                 </div>
             </div>

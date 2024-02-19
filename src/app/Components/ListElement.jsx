41a1d699596cb5623ac967d8b213fab691720b1e
Manuel Alanis <manuelaag99@gmail.com>
2024-02-19 15:11:03 -0600

    add state to edit button in admin

--- src/app/Components/ListElement.jsx
+++ src/app/Components/ListElement.jsx
@@ -1,7 +1,10 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
 
 export default function ListElement ({ element, elementClassNames, elementType, onClickFunction }) {
+    const [openWindowForEdit, setOpenWindowForEdit] = useState();
+
     return (
         <div className={"flex flex-col sm:flex-row w-full py-3 px-2 hover:bg-gray-300 duration-200 cursor-pointer h-fit " + elementClassNames} onClick={onClickFunction}>
             <div className="flex justify-center sm:justify-start w-full sm:w-8/10 py-1">
@@ -10,7 +13,7 @@ export default function ListElement ({ element, elementClassNames, elementType, 
                 </p>
             </div>
             <div className="flex flex-row w-full sm:w-2/10 my-1 sm:my-0">
-                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">
+                <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer" onClick={() => setOpenWindowForEdit(true)}>
                     <EditIcon color="black" />
                 </button>
                 <button className="w-1/2 justify-center text-center text-black hover:text-white duration-200 cursor-pointer">

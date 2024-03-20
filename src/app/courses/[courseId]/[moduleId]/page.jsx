91a4e3df5170229708508153373504c640595244
Manuel Alanís <manuelaag99@gmail.com>
2024-03-20 10:42:29 -0600

    functional logic to block users from seeing modules they havent bought

--- src/app/courses/[courseId]/[moduleId]/page.jsx
+++ src/app/courses/[courseId]/[moduleId]/page.jsx
@@ -29,27 +29,54 @@ export default function CourseVideoPage ({ params }) {
         }
     }
 
+    const [coursesThatTheUserHasPurchased, setCoursesThatTheUserHasPurchased] = useState();
+    const [hasUserPurchasedThisCourse, setHasUserPurchasedThisCourse] = useState(false);
+    async function fetchCoursesThatTheUserHasPurchased () {
+        try {
+            const { data, error } = await supabase.from("cai-purchased-courses").select().eq("user_id", auth.userId);
+            if (error) console.log (error);
+            setCoursesThatTheUserHasPurchased(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        if (coursesThatTheUserHasPurchased) {
+            coursesThatTheUserHasPurchased.some((course) => {
+                if (course.user_id === auth.userId) {
+                    setHasUserPurchasedThisCourse(true);
+                } else {
+                    setHasUserPurchasedThisCourse(false);
+                }
+            })
+        }
+    }, [coursesThatTheUserHasPurchased])
+
     useEffect(() => {
         fetchModuleInfo();
         fetchVideoComments();
+        fetchCoursesThatTheUserHasPurchased();
     }, [])
 
-    console.log(videoComments)
-
-    if (!moduleInfo) {
+    if (!moduleInfo || !coursesThatTheUserHasPurchased) {
         return null;
     } else {
         return (
             <div className="flex flex-col justify-center w-full ">
                 <TopNavigationBar />
                 <div className="flex flex-col justify-center sm:w-85percent w-95percent mx-auto bg-white rounded-md sm:mt-40 mt-20 mb-28 shadow-lg ">
-                    <div className="flex flex-col w-full px-10 py-7">
+                    {coursesThatTheUserHasPurchased && hasUserPurchasedThisCourse && <div className="flex flex-col w-full px-10 py-7">
                         <div className="flex w-full py-2 font-bold font-amatic text-black text-page-title-desktop -mt-6">{moduleInfo.title}</div>
                         <div className="flex w-full py-2 font-medium font-rubik text-gray-400">{moduleInfo.description}</div>
-                    </div>
-                    <div className="flex flex-col w-9/10 mx-auto mb-10 ">
+                    </div>}
+                    {coursesThatTheUserHasPurchased && hasUserPurchasedThisCourse && <div className="flex flex-col w-9/10 mx-auto mb-10 ">
                         <VideoPlayer additionalClassNames=" my-5" videoSource={moduleInfo.module_video_path} />
-                    </div>
+                    </div>}
+                    {coursesThatTheUserHasPurchased && !hasUserPurchasedThisCourse && <div>
+                        <p className="text-center text-black">
+                            Lo sentimos, no tienes acceso a esto porque no has comprado este curso.
+                        </p>
+                    </div>}
                     <div className="flex flex-col w-full px-10 py-3 h-fit mb-7">
                         <div className="flex w-full py-2">
                             <p className="font-bold">
